ff2ea38d979da405727728cc358ffeef36a0ed22
Juma <gerald.juma@student.moringaschool.com>
2025-03-04 11:28:06 +0300

    greetings

---
+++ js/intro-to-function.js
@@ -0,0 +1,12 @@
+function greetings(name){
+    console.log("Hello There! " + name );
+}
+//greetings Juma
+
+//arrow function
+
+//const greetings = (name) => {
+//    console.log("Hello" + name );
+// }
+
+greetings("Gerald")
